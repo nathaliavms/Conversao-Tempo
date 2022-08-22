a84b5ef345f46db4242ac47481192eb53f524004
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-22 12:08:35 -0300

    first commit

---
+++ Tempo/ConversaoDeTempo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tempo
+{
+    class ConversaoDeTempo
+    {
+        public int SegundosInformado { get; set; }
+        public int Hora { get; set; }
+        public int Minutos { get; set; }
+        public int Segundo { get; set; }
+        public int Resta { get; set; }
+        public ConversaoDeTempo (int segundosInformado)
+        {
+            SegundosInformado = segundosInformado;
+        }
+        public void Calculo()
+        {
+            Hora = SegundosInformado / 3600;
+            Resta = SegundosInformado % 3600;
+            Minutos = Resta / 60;
+            Segundo = Resta % 60;
+            Console.WriteLine(Hora + ":" + Minutos + ":" + Segundo);
+        }
+    }
+}
---
+++ Tempo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os segundos");
+            var segundosInformado = int.Parse(Console.ReadLine());
+            ConversaoDeTempo tempo = new ConversaoDeTempo(segundosInformado);
+            tempo.Calculo();
+        }
+    }
+}
